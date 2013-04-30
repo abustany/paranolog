7ec45161f33a9b8988054ddd18164a1fe243b263
Adrien Bustany <adrien@bustany.org>
2013-04-30 19:37:28 +0200

    Make application name prettier

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ main(int argc, char **argv)
     QApplication app(argc, argv);
 
     app.setOrganizationName(QString::fromLatin1("bustany.org"));
-    app.setApplicationName(QString::fromLatin1("paranolog"));
+    app.setApplicationName(QString::fromLatin1("ParanoLog"));
     app.setQuitOnLastWindowClosed(false);
 
     Paranolog log;

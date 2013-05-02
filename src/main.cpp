8ae1561e748940498933ac24f92d3f352793960c
Adrien Bustany <adrien@bustany.org>
2013-05-02 14:46:52 +0200

    Add UNIX signal handler to quit properly

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,37 @@
 
 #include <QtCore>
 
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/signalfd.h>
+
+static void
+setupUnixSignalHandler()
+{
+    int sigFd;
+
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGTERM);
+    sigaddset(&mask, SIGINT);
+
+    if (sigprocmask(SIG_SETMASK, &mask, NULL) == -1
+    || (sigFd = signalfd(-1, &mask, 0)) == -1) {
+        qWarning() << "Cannot setup UNIX signal handler: " << strerror(errno);
+        return;
+    }
+
+    QSocketNotifier *notifier = new QSocketNotifier(sigFd, QSocketNotifier::Read, qApp);
+    QObject::connect(notifier, SIGNAL(activated(int)), qApp, SLOT(quit()));
+}
+
 int
 main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+
+    setupUnixSignalHandler();
 
     app.setOrganizationName(QString::fromLatin1("bustany.org"));
     app.setApplicationName(QString::fromLatin1("ParanoLog"));
@@ -17,5 +44,9 @@ main(int argc, char **argv)
         return 1;
     }
 
-    return app.exec();
+    const int ret = app.exec();
+
+    qDebug() << "Exiting" << app.applicationName();
+
+    return ret;
 }
